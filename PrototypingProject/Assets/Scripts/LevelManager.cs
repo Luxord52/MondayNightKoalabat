20d4cc6f36861f717fefd5025a1fcbf401bb2e27
Liam Devinney <luxord52@gmail.com>
2019-11-16 14:47:41 -0500

    LevelManager, ScorePoint, and ScoreZone scripts, and their folders. Initial Push for them. - Liam

---
+++ PrototypingProject/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public int team1Score;
+    public int team2Score;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ScoreIncreased(int teamValue)
+    {
+        if (teamValue == 1)
+        {
+            team2Score++;
+        }
+        else
+        {
+            team1Score++;
+        }
+    }
+
+}
---
+++ PrototypingProject/Assets/Scripts/Scoring/ScorePoint.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScorePoint : MonoBehaviour
+{
+    public int scoreThreshold;
+    public GameObject levelManagerObject;
+    LevelManager levelManager;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        levelManagerObject = GameObject.FindWithTag("LevelManager");
+        levelManager = levelManagerObject.GetComponent<LevelManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void LateUpdate()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        IncreaseScore(collision.gameObject);
+    }
+
+    public void IncreaseScore(GameObject player)
+    {
+        StateManager playerStateManager = player.GetComponent<StateManager>();
+        int teamValue = playerStateManager.team;
+        levelManager.ScoreIncreased(teamValue);
+    }
+}
